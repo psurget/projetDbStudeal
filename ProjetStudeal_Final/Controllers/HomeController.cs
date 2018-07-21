597534e6cadb44985efc8c25b183a47c62ceb3ed
Pascal Surget <surget.pascal@gmail.com>
2018-07-21 14:31:27 -0400

    commit

--- ProjetStudeal_Final/Controllers/HomeController.cs
+++ ProjetStudeal_Final/Controllers/HomeController.cs
@@ -5,8 +5,10 @@ using ProjetStudeal_Final.Models;
 
 namespace ProjetStudeal_Final.Controllers
 {
+
     public class HomeController:Controller
     {
+
         DBStudealContext context = new DBStudealContext();
         
         public HomeController(DBStudealContext _context)
@@ -23,7 +25,6 @@ namespace ProjetStudeal_Final.Controllers
         /* ============== MEMBER METHODS =================*/
 
         /* ======== CONNECT ======== */
-        [ActionName("connect2")]
         public ActionResult Connect ( int ID )
         {
             Member m = context.Member.Find(ID);
@@ -35,17 +36,17 @@ namespace ProjetStudeal_Final.Controllers
             else
             {
                 ViewBag.login = m.Id;
-                return View("Details", m);
+                return View("MemberView", m);
             }           
         }
         /* ======== FIN CONNECT ======== */
 
-        /* ======== CREATE MEMBER ======== */
+        /* ======== CREATE NEW MEMBER ======== */
         [HttpGet]
        // [ActionName("Create")]
         public ActionResult CreateMember()
         {
-            return View("CreateMember");
+            return View("MemberForm");
         }
         [HttpPost]
         public ActionResult CreateMember([ Bind(include:"FirstName, LastName, UserName, Password")] Member m)
@@ -65,16 +66,38 @@ namespace ProjetStudeal_Final.Controllers
 
         /* ======== MEMBER EDIT ======== */
         [HttpGet]
-        public ActionResult MemberEdit(int Id)
+        public ActionResult MemberEdit(int id)
         {
-            Member m = context.Member.Find(Id);
-            return View(m);
+            int isTutor = context.Member.Find(id).IsTutor;
+            string body = "";
+            Member m = context.Member.Find(id);
+            List<Tutoring> tuto = context.Tutoring.Where(t => t.TutorId.Equals(id)).ToList();
+            if (isTutor.Equals(1))
+                {
+                    body = "<button asp-action='CreateTuto'>+ Ajouter un tutorat</button>";
+                    body += "<h3> Tutorats </h3>";
+
+                    
+                    if (tuto.Count > 0)
+                    {
+                        body += "<table border=1>";
+                        foreach (var t in tuto)
+                        {
+                            body += "<tr>";
+                            body += "<td><b>" + t.Subject + "</b></td>";
+                            body += "<td>" + t.TimeSlot + "</td>";
+                            body += "</tr>";
+                        }
+                        body += "</table>";
+                    }
+                }
+            return View("MemberForm", m);
         }
         [HttpPost]
         public ActionResult MemberEdit(int Id, Member m)
         {
-            context.Add(m);
-            return View(m);
+            context.Update(m);
+            return View("MemberView",m);
         }
         /* ======== FIN MEMBER EDIT ======== */
 
@@ -85,28 +108,44 @@ namespace ProjetStudeal_Final.Controllers
             return View(tuto);
         }
 
+        /* === Creer un tutorat a partir du membre ===*/
         [HttpGet]
-        public ActionResult CreateTuto()
+        public ActionResult CreateTuto(int tutorId)
         {
+            Member m = context.Member.Find(tutorId);
             Tutoring t = new Tutoring();
+            t.TutorId = tutorId;
             return View(t);
-        }         
+        } 
+
+        [HttpPost]
+        public ActionResult CreateTuto(int Id,[Bind(include:"Subject,TutorId")] Tutoring t)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(t);
+                return View(t);
+            }
+            return View();
+        }
 
         /* ======== INSCRIPTIONS ======== */
+        /* formulaire inscription*/
         [HttpGet]
         public ActionResult Inscription(int Id)
         {
             MeetingRequest mr = context.MeetingRequest.Find(Id);
             return View(mr);
         }
+        /* envoie inscription*/
         [HttpPost]
-        public ActionResult Inscription(int Id, [Bind(include: "State, StudentId, TimeSlotId")] MeetingRequest mr)
+        public ActionResult Inscription(int Id,[Bind(include:"State,StudentId,TimeSlotId")] MeetingRequest mr)
         {
             if (ModelState.IsValid)
             {
                 context.Add(mr);
                 context.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("MemberView", mr);
             }
             return View();
         }
